cbc398d396bbf876794310497b8058c023390cfa
Fiabio97 <giacomino@fabio.lan>
2021-10-28 09:12:24 +0200

    Creazione algoritmo

--- src/main/java/it/unical/ProvaINGSW/MyListUtil.java
+++ src/main/java/it/unical/ProvaINGSW/MyListUtil.java
@@ -5,22 +5,45 @@ import java.util.List;
 
 public class MyListUtil 
 {
-/*	
-	public List<Integer> sort(List<Integer> list, int order){
-		List<Integer> l = new ArrayList<Integer>();
-		
-		l = list;
-		
-		if(order == 0) {
-			
-		} else {
-			
-		}
-		return list;  
-    }
-/*
+	/*
     order = 0 //Ascendente
 
     order = 1 //Dicendente
- */  
+	*/  
+	public List<Integer> sort(List<Integer> list, int order){
+		
+		if(order < 0 || order > 1)
+			return null;
+		
+		if(list.size() == 0)
+			return list;
+		
+		List<Integer> l = new ArrayList<Integer>();
+		
+		l = list;
+
+		if(order == 0) {
+			for(int i = 0; i < l.size(); ++i) {
+				for(int j = i + 1; j < l.size(); ++j) {
+					if(l.get(j) < l.get(i)) {
+						Integer tmp = l.get(i);
+						l.set(i, l.get(j));
+						l.set(j, tmp);
+					}
+				}
+			}
+		} else {
+			for(int i = 0; i < l.size(); ++i) {
+				for(int j = i + 1; j < l.size(); ++j) {
+					if(l.get(j) > l.get(i)) {
+						Integer tmp = l.get(i);
+						l.set(i, l.get(j));
+						l.set(j, tmp);
+					}
+				}
+			}
+		}
+		return l;  
+    }
+
 }
