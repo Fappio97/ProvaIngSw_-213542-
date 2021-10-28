189c65aabec46126f7f50403b4e498dabdc9bc00
Fiabio97 <giacomino@fabio.lan>
2021-10-28 09:03:32 +0200

    Inizio prova

---
+++ src/main/java/it/unical/ProvaINGSW/MyListUtil.java
@@ -0,0 +1,26 @@
+package it.unical.ProvaINGSW;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListUtil 
+{
+/*	
+	public List<Integer> sort(List<Integer> list, int order){
+		List<Integer> l = new ArrayList<Integer>();
+		
+		l = list;
+		
+		if(order == 0) {
+			
+		} else {
+			
+		}
+		return list;  
+    }
+/*
+    order = 0 //Ascendente
+
+    order = 1 //Dicendente
+ */  
+}
---
+++ src/test/java/it/unical/ProvaINGSW/MyListUtilTest.java
@@ -0,0 +1,20 @@
+package it.unical.ProvaINGSW;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyListUtilTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
